20656b0b6f58e274cf6d39318236301f5528fa97
María Paz Beltrán <53879933+mariaPazBeltran@users.noreply.github.com>
2020-03-24 20:18:54 -0300

    Revert "Feature/menu"

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,31 +1,18 @@
-import React, {useContext, useEffect} from 'react'
+import React, {useContext} from 'react'
 import Product from '../components/Product'
-import {useHistory,
+import {
     Switch,
     Route,
     Link
   } from "react-router-dom";
-import './Style.css'
 import MyContext from '../states/MyContext'
-import Detail from '../components/Detail';
 
 const Menu = () =>{
     const {state} = useContext(MyContext)
-    let history = useHistory();
-    /*Al cargar la vista del menú, primero se 
-      visualizaran las opciones del desayuno
-     */
-    useEffect(() => {
-      history.push("/break");
-    },[history]);
-
     return(
-      <div id='menu-container'>
-        <Detail/>
-      <div id='options'>
-<button className='menu-options'><Link to='/break'>Breakfast</Link></button>
-<button className='menu-options'><Link to='/lunch'>Lunch</Link></button>
-<div id='products_buttons'>
+      <div>
+<Link to='/break'>brfas</Link>
+<Link to='/lunch'>lnch</Link>
 <Switch>
 <Route path="/break">
 {console.log(state.Products.Breakfast)}
@@ -40,8 +27,7 @@ const Menu = () =>{
            })}
 </Route>
 </Switch>
-</div>
-      </div>
+         
       </div>
     )
 }
